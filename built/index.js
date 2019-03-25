b9adaaa666de67af36f2fff0d8c4cc444e6927b2
Valvas Vincent <valvas.vincent@outlook.fr>
2019-03-25 16:08:11 +0100

    First commit

---
+++ built/index.js
@@ -0,0 +1,6 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+var pokemon_js_1 = require("./pokemon.js");
+/****************************************************************************************************/
+var newPokemon = new pokemon_js_1.Pokemon('Bulbizare');
+console.log(newPokemon.name);
---
+++ src/pokemon.ts
@@ -0,0 +1,35 @@
+/****************************************************************************************************/
+
+class Pokemon
+{
+  name: string
+  health: number
+  attack: number
+  defense: number
+  speed: number
+  specialDefense: number
+  specialAttack: number
+
+  /**************************************************/
+
+  constructor(name: string, health: number, attack: number, defense: number, speed: number, specialDefense: number, specialAttack: number)
+  {
+    this.name = name;
+    this.health = health;
+    this.attack = attack;
+    this.defense = defense;
+    this.speed = speed;
+    this.specialDefense = specialDefense;
+    this.specialAttack = specialAttack;
+  }
+
+  /**************************************************/
+
+  
+}
+
+/****************************************************************************************************/
+
+export { Pokemon };
+
+/****************************************************************************************************/
